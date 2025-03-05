ca283ffca86c8bd3d9d18c4219e4ea5c1e5434f7
ezpz44 <eliseoiii@gmail.com>
2025-03-05 13:30:13 -0600

    Initial commit from VS

---
+++ FileFixer.py
@@ -0,0 +1,13 @@
+# Display title, instructions, and accept user input for target file.
+
+# Offer manual selection of desired file type conversion.
+
+# Open file and examine magic bytes
+
+# Compare magice bytes to list of known formats and find closest match.
+
+# Replace magic bytes with closest match.
+
+# Save new copy seperately and preserve original.
+
+# Return to intro for repeated execution as needed.
